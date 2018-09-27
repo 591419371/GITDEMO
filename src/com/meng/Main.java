541f3e80b47e1be87a1c06efc1967563a878a330
59141 <591419371@qq.com>
2018-09-27 10:49:54 +0800

    提交一个hahaha

--- src/com/meng/Main.java
+++ src/com/meng/Main.java
@@ -3,6 +3,7 @@ package com.meng;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("hhahaha");
+        // write your code here
     }
 }

3bc4bd238f4ec033c72a5249a60d557fe44e1e60
59141 <591419371@qq.com>
2018-09-27 10:57:15 +0800

    提交一个aaaaa

--- src/com/meng/Main.java
+++ src/com/meng/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hhahaha");
         System.out.println("lalalala");
-        System.out.println("xixixixi");
+        System.out.println("aaaaaa");
         // write your code here
     }
 }
